af0aa11054848b261e6574c212ab5ee5080b54bc
DengFan <dfan@mail.ustc.edu.cn>
2022-08-26 16:31:10 +0800

    Update user/pingpong.c

--- user/pingpong.c
+++ user/pingpong.c
@@ -3,30 +3,40 @@
 
 int main(int argc, char *argv[])
 {
-    int p[2];
+    int p1[2];
+    int p2[2];
     char buf[10];
-    if (pipe(p) < 0)
+    if (pipe(p1) < 0)
     {
         fprintf(2, "pipe error");
+        exit(1);
+    }
+    if (pipe(p2) < 0)
+    {
+        fprintf(2, "pipe error");
+        exit(1);
     }
     int pid = fork();
     if (pid > 0)
     {
-        write(p[1], "a", 1);
-        close(p[1]);
-        sleep(1);
-        read(p[0], buf, 1);
-        close(p[0]);
-        fprintf(2, "%d: received pong\n", getpid());
+        close(p1[0]);
+        write(p1[1], "ping", strlen("ping"));
+        close(p1[1]);
+        close(p2[1]);
+        read(p2[0], buf, strlen("pong"));
+        close(p2[0]);
+        fprintf(2, "%d: received %s\n", getpid(), buf);
         exit(0);
     }
     else if (pid == 0)
     {
-        read(p[0], buf, 1);
-        close(p[0]);
-        fprintf(2, "%d: received ping\n", getpid());
-        write(p[1], "b", 1);
-        close(p[1]);
+        close(p1[1]);
+        read(p1[0], buf, strlen("ping"));
+        close(p1[0]);
+        fprintf(2, "%d: received %s\n", getpid(), buf);
+        close(p2[0]);
+        write(p2[1], "pong", strlen("pong"));
+        close(p2[1]);
         exit(0);
     }
     else
