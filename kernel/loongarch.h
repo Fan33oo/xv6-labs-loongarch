9b6ce4bd27965210d55dee2485d43c4c2cb30dd8
df <dfan@mail.ustc.edu.cn>
2022-09-20 15:35:54 +0800

    Update lab pgtbl

--- kernel/loongarch.h
+++ kernel/loongarch.h
@@ -197,6 +197,14 @@ r_csr_pgdl()
   return x;
 }
 
+static inline uint64
+r_csr_badv()
+{
+  uint64 x;
+  asm volatile("csrrd %0, 0x7" : "=r" (x) );
+  return x;
+}
+
 static inline void
 w_csr_pgdl(uint64 x)
 {
@@ -297,13 +305,13 @@ intr_off()
 #define PTE_NX (1UL << 62) //non executable
 #define PTE_NR (1L << 61) //non readable
 #define PTE_RPLV (1UL << 63) //restricted privilege level enable
-#define PTE_A (1UL << 60) //the access bit
+#define PTE_A (1UL << 60)
 
 #define PAMASK          0xFFFFFFFFFUL << PGSHIFT
 #define PTE2PA(pte) (pte & PAMASK)
 // shift a physical address to the right place for a PTE.
 #define PA2PTE(pa) (((uint64)pa) & PAMASK)
-#define PTE_FLAGS(pte) ((pte) & 0xE0000000000001FFUL)
+#define PTE_FLAGS(pte) ((pte) & 0xF0000000000001FFUL)
 
 // extract the three 9-bit page table indices from a virtual address.
 #define PXMASK          0x1FF // 9 bits
